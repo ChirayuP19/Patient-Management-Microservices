0e8c101d4e3f1b42e59d5e2d66337cea3b00417c
ChirayuP19 <chirayu010675@gmail.com>
2026-04-07 10:05:50 -0400

    feat: add Patient entity class
    - Define Patient JPA entity with id, name, dob,contact fields
    
    - Add field-level validation annotations
    
    - Configure table mapping and indexes

---
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,40 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @NotNull(message = "name is required")
+    @Size(min = 2,max = 35,message = "name character must be between 2 and 35")
+    private String name;
+
+    @NotNull(message = "email is required")
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "address is required")
+    @Size(min = 5,max = 100,message = "address must be have 5 character and max 100 character")
+    private String address;
+
+    @NotNull(message = "date of birth required")
+    private LocalDate dateOfBirth;
+
+    @NotNull(message = "register date is required")
+    private LocalDate registerDate;
+
+}
